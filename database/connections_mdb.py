85209166293204c50f024351ac5617355542f4e6
HMTD-Movies <111636041+HMTD-Movies@users.noreply.github.com>
2023-02-11 07:05:56 +0530

    Update connections_mdb.py

--- database/connections_mdb.py
+++ database/connections_mdb.py
@@ -36,7 +36,7 @@ async def add_connection(group_id, user_id):
             mycol.insert_one(data)
             return True
         except:
-            logger.exception('Some error occurred!', exc_info=True)
+            logger.exception('Some Error Occurred!', exc_info=True)
 
     else:
         try:
@@ -49,7 +49,7 @@ async def add_connection(group_id, user_id):
             )
             return True
         except:
-            logger.exception('Some error occurred!', exc_info=True)
+            logger.exception('Some Error Occurred!', exc_info=True)
 
         
 async def active_connection(user_id):
@@ -128,6 +128,6 @@ async def delete_connection(user_id, group_id):
             )
         return True
     except Exception as e:
-        logger.exception(f'Some error occurred! {e}', exc_info=True)
+        logger.exception(f'Some Error Occurred! {e}', exc_info=True)
         return False
 
