96ce749e550fae1becaf1e7399dd60c105eebf23
HMTD-Movies <111636041+HMTD-Movies@users.noreply.github.com>
2023-02-12 11:35:05 +0530

    Create banned.py

---
+++ plugins/banned.py
@@ -0,0 +1,40 @@
+from pyrogram import Client, filters
+from utils import temp
+from pyrogram.types import Message
+from database.users_chats_db import db
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from info import SUPPORT_CHAT
+
+async def banned_users(_, client, message: Message):
+    return (
+        message.from_user is not None or not message.sender_chat
+    ) and message.from_user.id in temp.BANNED_USERS
+
+banned_user = filters.create(banned_users)
+
+async def disabled_chat(_, client, message: Message):
+    return message.chat.id in temp.BANNED_CHATS
+
+disabled_group=filters.create(disabled_chat)
+
+
+@Client.on_message(filters.private & banned_user & filters.incoming)
+async def ban_reply(bot, message):
+    ban = await db.get_ban_status(message.from_user.id)
+    await message.reply(f'**Sorry 💁🏻 {message.from_user.mention}, You are Banned to Use Me. \nBan Reason :- {ban["ban_reason"]}**', message.from_user.mention)
+
+@Client.on_message(filters.group & disabled_group & filters.incoming)
+async def grp_bd(bot, message):
+    buttons = [[
+        InlineKeyboardButton('Support', url=f'https://t.me/{SUPPORT_CHAT}')
+    ]]
+    reply_markup=InlineKeyboardMarkup(buttons)
+    vazha = await db.get_chat(message.chat.id)
+    k = await message.reply(
+        text=f"**Chat Not Allowed 🐞\n\nMy 👨🏻‍✈️ Admins has Restricted me From Working Here..! If you Want to Know More About it Contact Support..\nReason :-** <code>{vazha['reason']}</code>.",
+        reply_markup=reply_markup)
+    try:
+        await k.pin()
+    except:
+        pass
+    await bot.leave_chat(message.chat.id)
