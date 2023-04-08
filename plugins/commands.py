bd9723454eb9186bc3f3a19f5747f8ebfeae406e
HMTD-Movies <111636041+HMTD-Movies@users.noreply.github.com>
2023-04-08 17:13:33 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -885,6 +885,33 @@ async def stickerid(bot, message):
     else: 
        await message.reply("<b>Oops !! Not a sticker file</b>")
 
+@Client.on_message(filters.group & filters.command(["tr"]))
+async def left(client,message):
+	if (message.reply_to_message):
+		try:
+			lgcd = message.text.split("/tr")
+			lg_cd = lgcd[1].lower().replace(" ", "")
+			tr_text = message.reply_to_message.text
+			translator = Translator()
+			translation = translator.translate(tr_text,dest = lg_cd)
+			try:
+				for i in list:
+					if list[i]==translation.src:
+						fromt = i
+					if list[i] == translation.dest:
+						to = i 
+				await message.reply_text(f"Translated from **{fromt.capitalize()}** To **{to.capitalize()}**\n\n```{translation.text}```")
+			except:
+			   	await message.reply_text(f"Translated from **{translation.src}** To **{translation.dest}**\n\n```{translation.text}```")
+      			
+				
+			
+		except :
+			print("error")
+	else:
+			 ms = await message.reply_text("**You can Use This Command by using reply to message**")
+			 await ms.delete()
+
 @Client.on_message(filters.private & filters.command(["translater"]))
 async def echo(client, message):
     reply = message.reply_to_message
