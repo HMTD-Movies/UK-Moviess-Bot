a047fde8803ba4dcde1774604a43e934d7eec08b
HMTD-Movies <111636041+HMTD-Movies@users.noreply.github.com>
2023-04-08 14:40:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -885,9 +885,6 @@ URLOPEN_API = os.environ.get("URLOPEN_API", "")
 reply_markup = InlineKeyboardMarkup(
         [[
         InlineKeyboardButton("Request", callback_data='https://t.me/TG_Karthik')
-        ]],
-        [[
-        InlineKeyboardButton("🚫 Close", callback_data='close_data')
         ]]
     )
 
