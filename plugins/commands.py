04c45060e5ff01c60dd993885bbf7f9f36f1760e
HMTD-Movies <111636041+HMTD-Movies@users.noreply.github.com>
2023-04-08 16:22:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -3,6 +3,7 @@ import logging
 import random
 import asyncio
 from urllib.parse import quote
+from googletrans import Translator
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery
@@ -14,6 +15,8 @@ import math
 import time
 import heroku3
 import requests
+from helper.database import set,unset ,insert
+
 from pyrogram import Client, filters, enums
 from database.users_chats_db import db
 from pyrogram import Client, filters, enums
@@ -21,6 +24,7 @@ from pyrogram.errors import ChatAdminRequired, FloodWait
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from database.ia_filterdb import Media, get_file_details, unpack_new_file_id, get_bad_files
 from database.users_chats_db import db
+from plugins.list import list
 from info import *
 from utils import get_settings, get_size, is_subscribed, save_group_settings, temp
 from database.connections_mdb import active_connection
@@ -880,6 +884,305 @@ async def stickerid(bot, message):
        await message.reply(f"**Sticker ID is**\n`{message.reply_to_message.sticker.file_id}`\n\n**Unique ID is **\n\n`{message.reply_to_message.sticker.file_unique_id}`", quote=True)
     else: 
        await message.reply("<b>Oops !! Not a sticker file</b>")
+
+            
+@Client.on_message(filters.private & filters.command(["translater"]))
+async def echo(client, message):
+	keybord1= InlineKeyboardMarkup( [
+        [   InlineKeyboardButton("Tamil",callback_data = "ta"),
+        ],
+        [    InlineKeyboardButton("Afrikaans", callback_data='af'),
+             InlineKeyboardButton("Albanian", callback_data='sq'),
+            InlineKeyboardButton("Amharic",callback_data ='am'),
+        ],
+        [   InlineKeyboardButton("Arabic", callback_data='ar'),
+        InlineKeyboardButton("Armenian", callback_data='hy'),      
+        InlineKeyboardButton("Azerbaijani",callback_data = 'az'),        
+        ],
+        [InlineKeyboardButton("Basque",callback_data ="eu"),
+        	 InlineKeyboardButton("Belarusian",callback_data ="be"),       	
+	InlineKeyboardButton("Bengali",callback_data="bn")],
+	
+	[InlineKeyboardButton("Bosnian",callback_data = "bs"),
+	InlineKeyboardButton("Bulgarian",callback_data ="bg"),
+	InlineKeyboardButton("Catalan",callback_data = "ca")
+	],
+	[ 
+	InlineKeyboardButton("Corsican",callback_data ="co"),
+	InlineKeyboardButton("Croatian",callback_data = "hr"),
+	InlineKeyboardButton("Czech", callback_data = "cs"),
+	],
+	[ InlineKeyboardButton("Danish",callback_data = "da"),
+	InlineKeyboardButton("Dutch",callback_data = "nl"),
+	InlineKeyboardButton("Esperanto",callback_data = "eo"),	 
+	],
+	[InlineKeyboardButton(" Next --->",callback_data = "page2")
+	]
+	] )
+	try:
+		code =find(int(message.chat.id))
+	except Exception as e:
+		await message.reply_text(f" Error : {e}\nclick /start ........")
+		return 
+		
+	if code :
+			try:
+				translator = Translator()
+				translation = translator.translate(message.text,dest = code)
+			except Exception as e:
+				await message.reply_text(f"Error : {e}")
+				return
+			try:
+					for i in list:
+						if list[i]==translation.src:
+							fromt = i
+						if list[i] == translation.dest:
+							to = i
+					await message.reply_text(f"Translated from **{fromt.capitalize()}** To **{to.capitalize()}**\n\n```{translation.text}```\n\n join @lntechnical")
+			except Exception as e:
+					await message.reply_text(f"Translated from **{translation.src}** To **{translation.dest}**\n\n```{translation.text}```\n\n join @lntechnical")
+	else:
+		await  message.reply_text("Select language 👇",reply_to_message_id = message.message_id, reply_markup =keybord1)
+
+@Client.on_callback_query()
+async def translate_text(bot,update):
+      keybord1= InlineKeyboardMarkup( [
+        [ 
+            InlineKeyboardButton("Afrikaans", callback_data='af'),
+             InlineKeyboardButton("Albanian", callback_data='sq'),
+            InlineKeyboardButton("Amharic",callback_data ='am'),
+        ],
+        [   InlineKeyboardButton("Arabic", callback_data='ar'),
+        InlineKeyboardButton("Armenian", callback_data='hy'),      
+        InlineKeyboardButton("Azerbaijani",callback_data = 'az'),        
+        ],
+        [InlineKeyboardButton("Basque",callback_data ="eu"),
+        	 InlineKeyboardButton("Belarusian",callback_data ="be"),       	
+	InlineKeyboardButton("Bengali",callback_data="bn")],
+	
+	[InlineKeyboardButton("Bosnian",callback_data = "bs"),
+	InlineKeyboardButton("Bulgarian",callback_data ="bg"),
+	InlineKeyboardButton("Catalan",callback_data = "ca")
+	],
+	[ 
+	InlineKeyboardButton("Corsican",callback_data ="co"),
+	InlineKeyboardButton("Croatian",callback_data = "hr"),
+	InlineKeyboardButton("Czech", callback_data = "cs"),
+	],
+	[ InlineKeyboardButton("Danish",callback_data = "da"),
+	InlineKeyboardButton("Dutch",callback_data = "nl"),
+	InlineKeyboardButton("Esperanto",callback_data = "eo"),	 
+	],
+	[InlineKeyboardButton(" Next --->",callback_data = "page2")
+	]
+	] )
+
+      keybord2= InlineKeyboardMarkup([
+           [InlineKeyboardButton("English",callback_data = "en"),
+           InlineKeyboardButton("Estonian",callback_data = "et"),
+           InlineKeyboardButton("Finnish",callback_data = "fi")
+           ],
+           [InlineKeyboardButton("French",callback_data = "fr"),
+           InlineKeyboardButton("Frisian",callback_data = "fy"),
+           InlineKeyboardButton("Galician",callback_data = "gl")
+           ],
+           [InlineKeyboardButton("Georgian",callback_data = "ka"),
+           InlineKeyboardButton("German",callback_data = "de"),
+           InlineKeyboardButton("Greek",callback_data = "el")
+           ],
+           [InlineKeyboardButton("Gujarati",callback_data = "gu"),
+           InlineKeyboardButton("Haitian Creole",callback_data = "ht"),
+           InlineKeyboardButton("Hausa",callback_data ="ha")
+           ],
+           [InlineKeyboardButton("Hindi",callback_data = "hi"),
+           InlineKeyboardButton("Hungarian",callback_data = "hu"),
+           InlineKeyboardButton("Icelandic",callback_data = "is")
+           ],
+           [InlineKeyboardButton("Igbo",callback_data = "ig"),
+           InlineKeyboardButton("Indonesian",callback_data = "id"),
+           InlineKeyboardButton("Irish",callback_data = "ga")
+           ],
+           [InlineKeyboardButton("<--- Back",callback_data = "page1"),
+           InlineKeyboardButton(" Next --->",callback_data = "page3"),
+           ]
+            ])
+		
+      keybord3 = InlineKeyboardMarkup([
+                [ InlineKeyboardButton("Italian",callback_data = "it"),
+                InlineKeyboardButton("Japanese",callback_data = "ja"),
+                InlineKeyboardButton("Javanese",callback_data = "jv")
+                ],
+                [InlineKeyboardButton("Kannada",callback_data = "kn"),
+                InlineKeyboardButton("Kazakh",callback_data = "kk"),
+                InlineKeyboardButton("Khmer",callback_data = "km")
+                ],
+                [InlineKeyboardButton("Kinyarwanda",callback_data = "rw"),
+                InlineKeyboardButton("Korean",callback_data ="ko"),
+                InlineKeyboardButton("Kurdish",callback_data = "ku")
+                ],
+                [ InlineKeyboardButton("Kyrgyz",callback_data ="ky"),
+                InlineKeyboardButton("Lao",callback_data = "lo"),
+                InlineKeyboardButton("Latin",callback_data = "la")
+                ],
+                [InlineKeyboardButton("Latvian",callback_data = "lv"),
+                InlineKeyboardButton('Lithuanian',callback_data ="lt"),
+                InlineKeyboardButton("Luxembourgish",callback_data = "lb")
+                ],
+                [InlineKeyboardButton("Macedonian",callback_data = "mk"),
+                InlineKeyboardButton("Malagasy",callback_data ="mg"),
+                InlineKeyboardButton("Malay",callback_data ="ms")
+                ],
+                [InlineKeyboardButton("<--- Back",callback_data = "page2"),
+                InlineKeyboardButton(" Next --->",callback_data = "page4")
+                ]
+              
+ 
+ ])
+
+      keybord4 = InlineKeyboardMarkup([
+          [InlineKeyboardButton("Malayalam",callback_data = "ml"),
+          InlineKeyboardButton("Maltese",callback_data = "mt"),
+          InlineKeyboardButton("Maori",callback_data = "mi")
+          ],
+          [InlineKeyboardButton("Marathi",callback_data = "mr"),
+          InlineKeyboardButton("Mongolian",callback_data = "mn"),
+          InlineKeyboardButton("Myanmar (Burmese)",callback_data = "my")
+          ],
+          [InlineKeyboardButton("Nepali",callback_data ="ne"),
+          InlineKeyboardButton("Norwegian",callback_data = "no"),
+          InlineKeyboardButton("Nyanja (Chichewa)",callback_data = "ny")
+          ],
+          [InlineKeyboardButton("Odia",callback_data = "or"),
+          InlineKeyboardButton("Pashto",callback_data = "ps"),
+          InlineKeyboardButton("Persian",callback_data = "fa"),
+          ],
+          [InlineKeyboardButton("Polish",callback_data = "pl"),
+          InlineKeyboardButton("Portuguese",callback_data = "pt"),
+          InlineKeyboardButton("Punjabi",callback_data = "pa"),
+          ],
+          [InlineKeyboardButton("Romanian",callback_data = "ro"),
+          InlineKeyboardButton("Russian",callback_data = "ru"),
+          InlineKeyboardButton("Samoan",callback_data= "sm"),
+          ],
+          [InlineKeyboardButton("<--- Back",callback_data = "page3"),
+          InlineKeyboardButton("Next --->",callback_data = "page5")
+          ]
+          
+ 
+ 
+ 
+ ])
+
+      keybord5 = InlineKeyboardMarkup([
+         [InlineKeyboardButton("Scots Gaelic",callback_data = "gd"),
+         InlineKeyboardButton("Serbian",callback_data = "sr"),
+         InlineKeyboardButton("Sesotho",callback_data = "st")
+         ],
+         [InlineKeyboardButton("Shona",callback_data ="sn"),
+         InlineKeyboardButton("Sindhi",callback_data ="sd"),
+         InlineKeyboardButton("Sinhala (Sinhalese)",callback_data = "si")
+         ],
+         [InlineKeyboardButton("Slovak",callback_data = "sk"),
+         InlineKeyboardButton("Slovenian",callback_data = "sl"),
+         InlineKeyboardButton("Somali",callback_data = "so")
+         ],
+         [InlineKeyboardButton("Spanish",callback_data = "es"),
+         InlineKeyboardButton("Sundanese",callback_data ="su"),
+         InlineKeyboardButton("Swahili",callback_data ="sw")
+         ],
+         [InlineKeyboardButton("Swedish",callback_data = "sv"),
+         InlineKeyboardButton("Tagalog (Filipino)",callback_data ='tl'),
+         InlineKeyboardButton("Tajik",callback_data = "tg")
+         ],
+         [InlineKeyboardButton("Tamil",callback_data = "ta"),
+         InlineKeyboardButton("Tatar",callback_data = "tt"),
+         InlineKeyboardButton("Telugu",callback_data = "te")
+         ],
+         [InlineKeyboardButton("<--- Back",callback_data = "page4"),
+         InlineKeyboardButton("Next --->",callback_data = "page6")
+         ]  ])
+
+
+
+
+      keybord6 =  InlineKeyboardMarkup([
+       [InlineKeyboardButton("Thai",callback_data = "th"),
+       InlineKeyboardButton("Turkish",callback_data = "tr"),
+       InlineKeyboardButton("!Not Valid",callback_data ="en")     
+       ],
+       [InlineKeyboardButton("Ukrainian",callback_data = "uk"),
+       InlineKeyboardButton("Urdu",callback_data = "ur"),
+       InlineKeyboardButton("Uyghur",callback_data ="ug")
+       
+       ],
+       [InlineKeyboardButton("Uzbek",callback_data = "uz"),
+       InlineKeyboardButton("Vietnamese",callback_data ="vi"),
+       InlineKeyboardButton("Welsh",callback_data = "cy")
+       
+       ],
+       [InlineKeyboardButton("Xhosa",callback_data = "xh"),
+       InlineKeyboardButton("Yiddish",callback_data = "yi"),
+       InlineKeyboardButton("Yoruba",callback_data = "yo")],
+       [InlineKeyboardButton("<--- Back",callback_data = "page5")
+       
+       ] ])
+      
+      
+      
+      tr_text = update.message.reply_to_message.text
+      cb_data = update.data
+      if cb_data== "page2":
+      	await update.message.edit("Select language 👇",reply_markup = keybord2)
+      elif cb_data == "page1":
+      	await update.message.edit("Select language 👇",reply_markup =keybord1)
+      elif cb_data =="page3":
+      	await update.message.edit("Select language 👇",reply_markup =keybord3)
+      elif cb_data == "page4":
+      	await update.message.edit("Select language 👇",reply_markup =keybord4)
+      elif cb_data =="page5":
+      	await update.message.edit("Select language 👇",reply_markup =keybord5)
+      elif cb_data =="page6":
+      	await update.message.edit("Select language 👇",reply_markup =keybord6)
+      else :
+      		try:
+      			translator = Translator()
+      			translation = translator.translate(tr_text,dest = cb_data)
+      		except Exception as e:
+      			await update.message.edit(f"Error : {e}")
+      			return
+      		try:
+      			for i in list:
+      				if list[i]==translation.src:
+      					fromt = i
+      				if list[i] == translation.dest:
+      					to = i 
+      			await update.message.edit(f"Translated from **{fromt.capitalize()}** To **{to.capitalize()}**\n\n```{translation.text}```\n\n Join @HMTD_Links")
+      		except Exception as e:
+      			await update.message.edit(f"Translated from **{translation.src}** To **{translation.dest}**\n\n```{translation.text}```\n\n Join @HMTD_Links")
+
+@Client.on_message(filters.private &filters.command(['unset']))
+async def unsetlg(client,message):
+	unset(int(message.chat.id))
+	await message.reply_text("Successfully removed custom default language")
+
+@Client.on_message(filters.private &filters.command(['set']))
+async def setlg(client,message):
+    	    user_id = int(message.chat.id)
+    	    insert(user_id)
+    	    text = message.text
+    	    textspit = text.split('/set')
+    	    lg_code = textspit[1]
+    	    if lg_code:
+    	    		cd = lg_code.lower().replace(" ", "")
+    	    		try:
+    	    			lgcd = list[cd]
+    	    		except:
+    	    			await message.reply_text("❗️ This language Not available in My List \n Or Check Your spelling 😉",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Check List 📑" ,url="https://raw.githubusercontent.com/lntechnical2/Google-Translater-/main/List/list.txt")]]))
+    	    			return
+    	    		set(user_id,lgcd)
+    	    		await message.reply_text(f"Successfully set custom default language **{cd}**")
+    	    else:
+    	    		await message.reply_text(" Please use this command with an argument. \n **For Example:/set English**",reply_markup=InlineKeyboardMarkup([[	InlineKeyboardButton("How To Use",url = "https://youtu.be/dUYvenXiYKE")]]))
 
 BITLY_API = os.environ.get("BITLY_API", "aa2132168583d283fb288625d9352f2c5835512a")
 CUTTLY_API = os.environ.get("CUTTLY_API", "bd3a3ab946d7598ee459331dac9e9568e3d66")
