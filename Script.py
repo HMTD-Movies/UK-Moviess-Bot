863d48e40d08a10311fe17e913b7b6e3dc9f3004
HMTD-Movies <111636041+HMTD-Movies@users.noreply.github.com>
2023-04-09 09:51:32 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -139,3 +139,5 @@ New Features ✨
 TRANSLATED_MSG = """<b>Choose The Language From Here That I Want to Translate.👇</b>"""
 
 REQ_TO_ADMIN = """<b>😒 Currently Unavailable to My Database or Not Released This Movie 🎥 ! We are Really Sorry for Inconvenience..!\n Have Patience..! Our Greatest 👨🏻‍✈️ Admins Will Upload This Movie 🎥 As Soon as Possible.!\n\nRequest to Our Greatest 👨🏻‍✈️ Admins</b>"""
+
+CREDITS = """**Credits Here**"""
