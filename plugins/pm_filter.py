4a41bb222950b60d419cf09b04a58a83a1628ec4
HMTD-Movies <111636041+HMTD-Movies@users.noreply.github.com>
2023-04-24 15:57:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -215,7 +215,6 @@ async def next_page(bot, query):
                 pass
             await query.answer()
 
-
 @Client.on_callback_query(filters.regex(r"^spolling"))
 async def advantage_spoll_choker(bot, query):
     _, user, movie_ = query.data.split('#')
