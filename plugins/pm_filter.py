990da03911bb71b08cbed9f28cee10adf7640f5a
HMTD-Movies <111636041+HMTD-Movies@users.noreply.github.com>
2023-04-24 19:24:55 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -215,9 +215,10 @@ async def next_page(bot, query):
                 await query.edit_message_reply_markup(
                     reply_markup=InlineKeyboardMarkup(btn)
                 )
-            except MessageNotModified:
-                pass
-            await query.answer()
+        except MessageNotModified:
+            pass
+        await query.answer()
+
 
 @Client.on_callback_query(filters.regex(r"^spolling"))
 async def advantage_spoll_choker(bot, query):
